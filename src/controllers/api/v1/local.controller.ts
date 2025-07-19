2499d6da9e7e9f759c84a0770792c27f55a6ff6f
Himanshu Chuchra <code@himanshuchuchra.com>
2025-07-19 19:55:29 +0530

    Write Sign Up Controller Action

--- src/controllers/api/v1/local.controller.ts
+++ src/controllers/api/v1/local.controller.ts
@@ -1,13 +1,45 @@
 // Import Section
 import { type Response, type Request } from "express";
 import { APIError, APIResponse, asyncHandler } from "../../../handlers";
+import { User } from "../../../models";
 
 // Controller Actions
-const signup = asyncHandler(async (req: Request, res: Response) => {});
+const signup = asyncHandler(async (req: Request, res: Response) => {
+  const { username, email, password, rememberMe } = req.body;
+
+  const existingUser = await User.findOne({
+    $or: [{ email }, { username }],
+  }).lean();
+
+  if (existingUser) {
+    const isEmailTaken = existingUser.email === email;
+    const message = isEmailTaken
+      ? "Email already exists, Please Sign-In"
+      : "Username already exists, Please choose another";
+
+    return res.status(409).json(APIError.send(409, message));
+  }
+
+  await User.create({
+    email,
+    username,
+    password,
+    rememberMe,
+  });
+
+  return res
+    .status(201)
+    .json(APIResponse.send(201, "User registered successfully"));
+});
+
 const signin = asyncHandler(async (req: Request, res: Response) => {});
+
 const signout = asyncHandler(async (req: Request, res: Response) => {});
+
 const forgetPassword = asyncHandler(async (req: Request, res: Response) => {});
+
 const changePassword = asyncHandler(async (req: Request, res: Response) => {});
+
 const changeUsername = asyncHandler(async (req: Request, res: Response) => {});
 
 // Export Section
